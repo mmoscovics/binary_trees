d31967adcb799d0ae45ad83835d555bd2c076d0e
Mitchell <mmoscovics@yahoo.com>
2020-03-09 21:44:40 +0000

    [Update] betty compliance

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,15 +11,15 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new;
+	binary_tree_t *new;
 
-    new = malloc(sizeof(binary_tree_t));
-    if (new == NULL)
-        return (NULL);
-    new->n = value;
-    if (parent == NULL)
-        parent = new;
-    else
-        new->parent = parent;
-    return (new);
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = value;
+	if (parent == NULL)
+		parent = new;
+	else
+		new->parent = parent;
+	return (new);
 }
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,20 +11,20 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new;
+		binary_tree_t *new;
 
-    if (parent == NULL)
-        return (NULL);
-    new = malloc(sizeof(binary_tree_t));
-    if (new == NULL)
-        return (NULL);
-    new->n = value;
-    new->parent = parent;
-    if (parent->right != NULL)
-    {
-        new->right = parent->right;
-        parent->right->parent = new;
-    }
-    parent->right = new;
-    return (new);
+		if (parent == NULL)
+			return (NULL);
+		new = malloc(sizeof(binary_tree_t));
+		if (new == NULL)
+			return (NULL);
+		new->n = value;
+		new->parent = parent;
+		if (parent->right != NULL)
+		{
+			new->right = parent->right;
+			parent->right->parent = new;
+		}
+		parent->right = new;
+		return (new);
 }
