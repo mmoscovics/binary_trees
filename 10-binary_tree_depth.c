cdc100bc34166f7e65febe29833a1ac42f22bca3
Mitchell <mmoscovics@yahoo.com>
2020-03-10 17:37:46 +0000

    [Create] task 10 depth of tree, prototype in header

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures the height of a binary tree
+ *
+ * @tree: pointer to the root node of the tree
+ *
+ * Return: the height of the node, or 0
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->parent)
+	{
+		depth = binary_tree_depth(tree->parent);
+		return (depth + 1);
+	}
+	return (0);
+}
