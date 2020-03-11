ac02a93811c6d52801ac4347867052ef3a978a6c
Mitchell <mmoscovics@yahoo.com>
2020-03-11 16:30:32 +0000

    [Bugfix] restructure and add pointers

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -20,11 +20,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	new->n = value;
 	new->parent = parent;
-	if (parent->left != NULL)
-	{
-		new->left = parent->left;
-		parent->left->parent = new;
-	}
+	new->left = parent->left;
+	new->right = NULL;
 	parent->left = new;
+	if (new->left != NULL)
+		new->left->parent = new;
 	return (new);
 }
